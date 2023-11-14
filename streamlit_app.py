6233c2219a6906d0b1b08654d5d77c98e6c75661
testbabak <137612934+testbabak@users.noreply.github.com>
2023-11-14 15:39:41 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie ')
 streamlit.text(' 🐔 Hard-Bioled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
